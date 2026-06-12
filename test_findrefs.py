cca9f0c2b8ef7298878a4962cde2bd231f913ec7
MG1937 <2586364982@qq.com>
2026-06-12 18:42:25 +0800

    fix unix format crlf issue

--- test_findrefs.py
+++ test_findrefs.py
@@ -4,13 +4,12 @@ import mmap
 
 import time
 t_start = time.perf_counter()
-f = open("./classes5.dex","rb")
+f = open("./classes.dex","rb")
 mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
 buf = memoryview(mm)
-dex = DEX.parse(buf, "classes5.dex")
+dex = DEX.parse(buf, "classes.dex")
 insn_locator = InsnLocator(dex)
 insn_locator.parse()
 t_end = time.perf_counter()
 print(f"[DEBUG] insn_locator Time: {(t_end - t_start)*1000000:.2f} us")
-print(insn_locator.insn_offs)
 
