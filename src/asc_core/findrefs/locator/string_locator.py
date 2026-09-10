2036e0d7b9a00c859bfd2300df6cebf9f1300d09
ZacharyZcR <zacharyzcr1984@gmail.com>
2026-09-10 18:23:36 +0800

    Fix reference lookup and isolate decompiler dependencies

--- src/asc_core/findrefs/locator/string_locator.py
+++ src/asc_core/findrefs/locator/string_locator.py
@@ -1,5 +1,7 @@
 from findrefs.locator.base_locator import BaseLocator
 import struct
+from bisect import bisect_right
+from utils.leb128 import read_uleb128_len
 import re
 import time
 
@@ -14,6 +16,7 @@ class StringLocator(BaseLocator):
         self.stridx_map = {} # {string_data_off : string_idx}
         self.strdata_start = 0
         self.strdata_end = 0
+        self.string_offsets = []
         self.parsed = False
     
     def _build_map(self):
@@ -23,13 +26,15 @@ class StringLocator(BaseLocator):
         string_ids_off, string_ids_size = self.header.strings
         stridx_map = self.stridx_map
         buf = self.buf
-        # might buggy.. r8 not specify the first string data off is the begging of all string data off, but usually it was...
-        self.strdata_start = _STRUCT_I.unpack_from(buf, string_ids_off)[0]
         for idx in range(string_ids_size):
             data_offset = _STRUCT_I.unpack_from(buf, string_ids_off)[0]
             string_ids_off += 4
             stridx_map[data_offset] = idx
-        self.strdata_end = data_offset
+        self.string_offsets = sorted(stridx_map)
+        if self.string_offsets:
+            self.strdata_start = self.string_offsets[0]
+            last = self.string_offsets[-1]
+            self.strdata_end = buf.obj.find(b'\x00', last + read_uleb128_len(buf, last)) + 1
         self.parsed = True
         self._debug_log("build_map", t_start, len(stridx_map))
 
@@ -43,22 +48,24 @@ class StringLocator(BaseLocator):
         mm = submem.obj
         pattern = re.compile(string)
         
-        # offsets = []
         for match in pattern.finditer(submem):
-            offset = strdata_start + match.end()
-            offset = mm.find(b'\x00', offset, strdata_end)
-            yield offset + 1
-            # offsets.append(offset + 1) # skip over 00 byte
-        # return offsets
+            start = strdata_start + match.start()
+            item = self.string_offsets[bisect_right(self.string_offsets, start) - 1]
+            content_start = item + read_uleb128_len(buf, item)
+            content_end = mm.find(b'\x00', content_start, strdata_end)
+            if start >= content_start and strdata_start + match.end() <= content_end:
+                yield item
 
     def locate(self, string : str) -> set:
         t_start = time.perf_counter() if self.debug else None
         if not self.parsed:
             self._build_map()
+        if not self.string_offsets:
+            return set()
         stridx_map = self.stridx_map
         located_idx = set()
         for offset in self._match_string_offset(string):
-            located_idx.add(stridx_map[offset] - 1)
+            located_idx.add(stridx_map[offset])
         # return set for O(1) lookup
         self._debug_log("locate", t_start, len(located_idx))
         return located_idx
